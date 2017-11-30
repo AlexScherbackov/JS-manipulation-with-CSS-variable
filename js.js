c6788ecd36e4c0e5b5c31b41fae26c0ae983172a
AlexScherbakov <alsherbakov13@gmail.com>
2017-11-30 14:40:08 +0300

    basic module

--- js.js
+++ js.js
@@ -1,15 +1,39 @@
 'use strict';
 
-'use strict'
+let App =  {};
 
-const inputs = document.querySelectorAll('.control__input');
+App.define = function(namespace){
+	let parts = namespace.split('.'),
+			parent = App, 
+			i;
+	
+	if(parts[0] == 'App'){
+		parts = parts.slice(1);
+	}
 
-function handleUpdate(e){
-	const suf = this.dataset.sizing || '';
-	console.log(e.target);
-	document.getElementById('actionTarget').style.setProperty(`--${this.name}`, this.value + suf);
-  
+	for(i = 0; i < parts.length; i++){
+		if(typeof parent[parts[i]] == 'undefined'){
+			parent[parts[i]] = {}
+		}
+		parent =  parent[parts[i]];
+	}
+	return parent;
+
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+App.define('utils');
+
+App.utils = function(classControl, target){
+	const inputs = document.querySelectorAll(classControl);
+
+	function handleUpdate(e){
+		const suf = this.dataset.sizing || '';
+		console.log(e.target);
+		document.getElementById(target).style.setProperty(`--${this.name}`, this.value + suf);
+  }
+
+	inputs.forEach(input => input.addEventListener('change', handleUpdate));
+	inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+}
+
+App.utils('.control__input', 'actionTarget');
